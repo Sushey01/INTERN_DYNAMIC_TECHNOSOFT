b28d3c901f1119500c001c10dd0222b2325657b7
Sushey01 <susmagar012@gmail.com>
2025-04-25 10:57:06 +0545

    phase2

--- DAY8/musicrecommendation/src/App.jsx
+++ DAY8/musicrecommendation/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 //Created an array of friends.
@@ -18,7 +19,7 @@ const initialFriends = [
   {
     id: 2,
     name: "Sandip",
-    image: "https://i.pravatar.cc/40",
+    image: "https://i.pravatar.cc/41",
     recBalance: 10,
     recommendations: [
       { id: "song3", title: "National Anthem", artist: "Kavi Pradeep" },
@@ -30,7 +31,7 @@ const initialFriends = [
     id: 3,
 
     name: "Susan",
-    image: "https://i.pravatar.cc/40",
+    image: "https://i.pravatar.cc/42",
     recBalance: -3,
     recommendations: [
       { id: "song5", title: "Hawa Jastai Malai", artist: "John Chamling" },
@@ -41,7 +42,7 @@ const initialFriends = [
   {
     id: 4,
     name: "Ashish",
-    image: "https://i.pravatar.cc/40",
+    image: "https://i.pravatar.cc/43",
     recBalance: 0,
     recommendations: [
       { id: "song7", title: "Tum Hi Ho", artist: "Arijit Singh" },
@@ -51,13 +52,16 @@ const initialFriends = [
 ];
 
 export default function App() {
-  // const [friends, setFriends] = useState(initialFriends);
-  // const [selectedFriend, setSelectedFriend] = useState(null);
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+  const [selectedFriend, setSelectedFriend] = useState(null);
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList/>
-        <FormAddFriend />
+        {showAddFriend && <FormAddFriend />}
+        <FormAddSong />
+        <Button> Add Friend </Button>
       </div>
     </div>
   )
@@ -139,8 +143,8 @@ function Friend({friend}) {
 
 }
 
-function Button( {children}) {
-  return <button className="button">{children}</button>
+function Button( {children, onClick}) {
+  return <button className="button" onClick={onClick}>{children}</button>
 }
 
 function FormAddFriend(){
@@ -150,10 +154,33 @@ function FormAddFriend(){
 
     <label>📷😁Image URL</label>
     <input type="text" />
+    
 
-    <Button> Add Friend </Button>
+    <Button> Add </Button>
   </form>
 }
 
 
 
+function FormAddSong() {
+  return 
+  <form className="form-add-song">
+    <h2>RECOMMEND A SONG !</h2>
+    <label>🎵Song Name</label>
+    <input type="text" />
+
+    <label>🎤Artist Name</label>
+    <input type="text" />
+
+
+    <label>💿Who is recommending the song</label>
+    <select>
+      <option value="user1">Kushal</option>
+      <option value="user2"> Sandip </option>
+      <option value="user3">Susan</option>
+      <option value="user4">Ashish</option>
+    </select>
+
+    <Button>Check Recommendations </Button>
+  </form>
+}
